0f5adfa1c1ff934d9e02f3246626b3df9c899790
vickyasepitome <85425821+vickyasepitome@users.noreply.github.com>
2021-06-06 10:21:02 +0530

    Add files via upload

---
+++ Components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className = "note">
+      <h1> Javascript and React.js </h1>
+      <p>
+        This was an amazing bootcamp by Mr.Shaurya Sinha.
+        We covered everything from Scratch including Javascript
+        ,React.js and HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import "./styles.css";
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
